f88e560eb3e13e9f34d9e21fecbdf23647f54dab
sainidivya <sainid@stanford.edu>
2016-02-26 10:28:33 -0800

    more matches done

--- match_1.py
+++ match_1.py
@@ -1,12 +1,14 @@
 import re
 
-for x in range(30):
-	testerString = open("output."+(x+1)+".txt")
+for x in range(2):
+	testerString = open("output."+str(x+1)+".txt")
+	filename = "output."+str(x+1)+".txt" 
 	for string in testerString:
 		if ("S&P" or "STANDARD & POOR") in string:
-			m = re.findall('\"([A-D][A-D]?[A-D][-+]?)\"', string)
-		if m != None:
-			for match in m:
-				print match
+		#	string = string.replace(u'\xe2', '\"')	
+			m = re.findall('\"([A-D][A-D]?[A-D]?[-+]?)\"', string)
+			if m != None:
+				for match in m:
+					print filename+": " + match
 
 
