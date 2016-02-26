5acc0d36d3c5feebac6348db98fffa74eab0150e
Catherina Xu <catherinaxu@stanford.edu>
2016-02-26 09:39:39 -0800

    changed

--- tag_1.py
+++ tag_1.py
@@ -1,7 +1,6 @@
 from nltk.tag import StanfordNERTagger
 #from nltk.tag.stanford import NERTagger
 
-<<<<<<< HEAD
 st = NERTagger('/Users/catherinaxu/Dropbox/Stanford/EC/Neighborly/neighborly-serenity/english.all.3class.distsim.crf.ser.gz','/Users/catherinaxu/Dropbox/Stanford/EC/Neighborly/neighborly-serenity/stanford-ner.jar')
 pdf = open('output.1.txt')
 for line in pdf:
@@ -11,4 +10,3 @@ for line in pdf:
 =======
 st = StanfordNERTagger('/Users/divya/Desktop/neighborly-serenity/english.all.3class.distsim.crf.ser.gz','/Users/divya/Desktop/neighborly-serenity/stanford-ner.jar')
 print(st.tag("I love to eat food at Stanford University with Professor John Smith".split()))
->>>>>>> origin/master
