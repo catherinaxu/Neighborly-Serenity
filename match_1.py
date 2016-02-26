fbd97acea4e5ceeb24cf897f595bad79c5e4fbb3
sainidivya <sainid@stanford.edu>
2016-02-26 12:53:38 -0800

    quotations fixed

--- match_1.py
+++ match_1.py
@@ -1,8 +1,8 @@
 import re
 
-for x in range(2):
+for x in range(6):
 	testerString = open("output."+str(x+1)+".txt")
-	filename = "output."+str(x+1)+".txt" 
+	filename = "output."+str(x+1)+".txt"
 	for string in testerString:
 		if ("S&P" or "STANDARD & POOR") in string:
 		#	string = string.replace(u'\xe2', '\"')	
