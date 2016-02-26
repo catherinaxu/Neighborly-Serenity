0e8a611c158eb747759cab66628df8e36a7c983b
sainidivya <sainid@stanford.edu>
2016-02-26 09:58:45 -0800

    loop through files

--- match_1.py
+++ match_1.py
@@ -1,11 +1,12 @@
 import re
 
-testerString = open("output.1.txt")
-for string in testerString:
-	if "S&P" in string:
-		m = re.findall('\"([A-D][A-D]?[A-D][-+]?)\"', string)
-	if m != None:
-		for match in m:
-			print match
+for x in range(30):
+	testerString = open("output."+(x+1)+".txt")
+	for string in testerString:
+		if ("S&P" or "STANDARD & POOR") in string:
+			m = re.findall('\"([A-D][A-D]?[A-D][-+]?)\"', string)
+		if m != None:
+			for match in m:
+				print match
 
 
